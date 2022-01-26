10ea059a9b5dc0602dfc634dd3e2d95cb191f332
Jake Finn <lof.steven@gmail.com>
2022-01-26 17:21:13 -0600

    Add fsUtils.js to helpers folder, refactor css links for index.html and notes.html, rename db to notes_db

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+// const { clog } = require('./middleware/clog');
+// const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/assets/pages/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
